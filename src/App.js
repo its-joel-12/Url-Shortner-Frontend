a31504b66a78b472da6688de3cb54e091d247f41
Joel Oliveira <joel80851@gmail.com>
2024-08-01 18:24:36 +0530

    tried to collect characters after slash

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes,Route, useLocation } from 'react-router-dom';
+import React, { useEffect } from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to the Home Page</h1>
+      {/* <Link></Link> */}
+    </div>
+  );
+};
+
+const NotFound = () => {
+  const location = useLocation();
+  const path = location.pathname;
+  const lastSegment = path.substring(path.lastIndexOf('/') + 1);
+  console.log(lastSegment);
+
+// useEffect(function () =>{
+
+// }[])
+
+  return (
+    <div>
+      <h2>404 Page Not Found</h2>
+      <p>The path you entered: {path}</p>
+      
+    </div>
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
   );
 }
 
